d065a96d32295e3583b20b99c0211d05ee3b631d
Jonathan Horn <jonathan@autoit4you.de>
2019-01-03 22:23:56 +0100

    Fix notification script

--- vars/sendNotification.groovy
+++ vars/sendNotification.groovy
@@ -9,17 +9,17 @@ def call(String buildStatus = 'STARTED') {
 
 	// Default values
 	def colorCode = '#FF0000'
-  	def summary = "${subject} (${env.BUILD_URL})"
+  def summary = "${env.JOB_NAME} - #${env.BUILD_NUMBER}"
   	
-  	// Override default values based on build status
-  	if (buildStatus == 'STARTED') {
-    	color = 'YELLOW'
-    	colorCode = '#FFFF00'
-  	} else if (buildStatus == 'SUCCESSFUL') {
-    	color = 'GREEN'
-    	colorCode = '#00FF00'
-  	}
+  // Override default values based on build status
+  if (buildStatus == 'STARTED') {
+    color = 'YELLOW'
+    colorCode = '#FFFF00'
+  } else if (buildStatus == 'SUCCESSFUL') {
+    color = 'GREEN'
+    colorCode = '#00FF00'
+  }
 
-  	// Send notifications
-  	slackSend (color: colorCode, message: summary)
+  // Send notifications
+  slackSend (color: colorCode, message: summary)
 }
