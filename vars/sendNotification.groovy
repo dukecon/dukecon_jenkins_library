ee0de360f3c44893f0f171dd8af21b4d9e2844a7
Jonathan Horn <jonathan@autoit4you.de>
2019-01-03 22:32:39 +0100

    Further improvement

--- vars/sendNotification.groovy
+++ vars/sendNotification.groovy
@@ -19,6 +19,11 @@ def call(String buildStatus = 'STARTED') {
   } else if (buildStatus == 'SUCCESSFUL') {
     color = 'GREEN'
     colorCode = '#00FF00'
+    summary += "Success after x "
+  } else if (buildStatus) {
+    color = 'RED'
+    colorCode = '#FF0000'
+    summary += "Failed after x "
   }
 
   summary += "(<${env.BUILD_URL}|Open>)"
