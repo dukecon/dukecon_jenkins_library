aea78c718bceaca9791fa9930e8fe0e076068a49
Jonathan Horn <jonathan@autoit4you.de>
2019-01-03 22:29:38 +0100

    Extend notification script

--- vars/sendNotification.groovy
+++ vars/sendNotification.groovy
@@ -9,16 +9,19 @@ def call(String buildStatus = 'STARTED') {
 
 	// Default values
 	def colorCode = '#FF0000'
-  def summary = "${env.JOB_NAME} - #${env.BUILD_NUMBER}"
+  def summary = "${env.JOB_NAME} - #${env.BUILD_NUMBER} "
   	
   // Override default values based on build status
   if (buildStatus == 'STARTED') {
     color = 'YELLOW'
     colorCode = '#FFFF00'
+    summary += "Starting build "
   } else if (buildStatus == 'SUCCESSFUL') {
     color = 'GREEN'
     colorCode = '#00FF00'
   }
+
+  summary += "(<${env.BUILD_URL}|Open>)"
 
   // Send notifications
   slackSend (color: colorCode, message: summary)
