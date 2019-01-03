3b398b0e2f305ba8e99cb71bdabfc49061cc8706
Jonathan Horn <jonathan@autoit4you.de>
2019-01-03 23:17:39 +0100

    Get duration from currentBuild

--- vars/sendNotification.groovy
+++ vars/sendNotification.groovy
@@ -10,7 +10,7 @@ def call(String buildStatus = 'STARTED') {
 	buildStatus =  buildStatus ?: 'SUCCESSFUL'
 
 	// Default values
-  def duration = Thread.currentThread().executable.getDurationString()
+  def duration = currentBuild.durationString
 	def colorCode = '#FF0000'
   def summary = "${env.JOB_NAME} - #${env.BUILD_NUMBER} "
   	
